d67908b548427d344dcd9ecf0b3c2c367f4db225
UriyRatinskiy <uriy_mark@ukr.net>
2023-03-09 21:17:47 +0200

    task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+
+const ulCategories = document.querySelector('#categories');
+
+items.forEach(function (item){
+  this.item === li.item;
+console.log(`Number of categories: ${this.item}`);
+});
+
+
+// const liItems = document.querySelector('.item');
+//  forEach(function (h2, li) {
+  
+//  console.log(`Category: ${h2}, Elements: ${li}`);
+// });
